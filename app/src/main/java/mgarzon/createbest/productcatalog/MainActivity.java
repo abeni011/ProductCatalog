ddbf4d3488008d4649a1b5a8b0c83b5cf0a3e814
Alex <abeni011@uottawa.ca>
2020-11-25 16:18:33 -0500

    comment

--- app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
+++ app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
@@ -25,6 +25,8 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+//checking for push success
+
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextName;
